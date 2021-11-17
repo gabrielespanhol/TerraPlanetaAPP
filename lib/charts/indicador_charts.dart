d45e195181901a1bb9a0d5ed563d1111582b33f3
Your Name <gabryel.ehs@gmail.com>
2021-11-17 13:43:37 -0300

    c

--- lib/charts/indicador_charts.dart
+++ lib/charts/indicador_charts.dart
@@ -1,22 +1,27 @@
-import 'package:flutter/cupertino.dart';
-import 'package:thefive_planetaagua/charts/indicador_series.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/cupertino.dart';
 
 
+import 'indicador_series.dart';
+
+// ignore: must_be_immutable
 class IndicadorCharts extends StatelessWidget {
-  final List<IndicadorSeries> data;
 
-  IndicadorCharts({this.data});
+   final List<IndicadorSeries> dados;
 
-  List<charts.Series<IndicadorSeries, String>> series = [
+  IndicadorCharts({this.dados});
+
+   List<charts.Series<IndicadorSeries, String>> series = [
 
     charts.Series(
       id: "Indicadores",
-      data: data,
+      data: dados,
       domainFn: (IndicadorSeries series, _) => series.year,
       measureFn: (IndicadorSeries series, _) => series.ph,
+      colorFn: (IndicadorSeries series, _) => series.barColor
     )
   ];
+
 
   @override
   Widget build(BuildContext context) {
